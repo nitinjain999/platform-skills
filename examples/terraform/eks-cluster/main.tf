04021903b2eb38411bae8b90ee6fdc3ee3e78c14
Nitin Jain <nitin.jain@atg.se>
2026-04-02 10:15:54 +0200

    First Release

--- examples/terraform/eks-cluster/main.tf
+++ examples/terraform/eks-cluster/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
-  }
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
